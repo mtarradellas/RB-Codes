1bb3e0708e1f1e4deab180e4e97a1508cdab0a90
Manuel Tarradellas <mtarradellas@itba.edu.ar>
2018-03-15 22:11:06 -0300

    a

--- oo.rb
+++
@@ -1,8 +0,0 @@
-my_array = [9, 5, 1, 2, 3, 4, 0, -1]
-
-p my_array.first(4)
-p my_array.drop(4)
-p my_array.prepend(10, 11)
-p my_array.append(20, 21)
-p my_array.map { |e| e * 3 }
-p my_array.map{ |e| e * 3 }.reduce { |sum, e| sum + e }
